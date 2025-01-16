ccde4dd534066685499c0bf7503565c5bcb673ea
DorianNet93 <lifebsfg@gmail.com>
2025-01-16 15:34:14 +0200

    fixcomp

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import Feedback from './components/Feedback/Feedback';
+import Feedback from './componentsssssss/Feedback/Feedback';
 import './App.css';
 
 const App = () => {
